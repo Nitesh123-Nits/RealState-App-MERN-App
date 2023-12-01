8c4327964d274ef3c9f9080d93cd549360828f37
Niteshnitian <67790419+Nitesh123-Nits@users.noreply.github.com>
2023-12-01 17:51:23 +0530

    "Added body and saved data to mongoDb database"

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 dotenv.config();
 const app=express();
 
@@ -9,7 +10,7 @@ mongoose.connect(process.env.MONGODB).then(()=>{
   console.log("MongoDb Connected..")
 }).catch(e => console.log(e))
 
-
+app.use('/api/user',userRouter)
 
 
 // App listenning on port 3000
