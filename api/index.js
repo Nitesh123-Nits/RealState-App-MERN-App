3240f2a7dfef36ec2e8576cd905a5d7d0d6fdf65
Niteshnitian <67790419+Nitesh123-Nits@users.noreply.github.com>
2023-12-17 12:08:38 +0530

    create listing route

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
-
+import listingRouter from './routes/listing.route.js'
 dotenv.config();
 const app=express();
 app.use(express.json())
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGODB).then(()=>{
 // Routes
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+app.use('/api/listing',listingRouter)
 
 // middleware
 app.use((err,req,res,next)=>{
