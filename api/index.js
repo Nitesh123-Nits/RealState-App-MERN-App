875bc0b58c1b0b4eecec5a63c54421e7783d371f
Niteshnitian <67790419+Nitesh123-Nits@users.noreply.github.com>
2023-12-24 17:20:27 +0530

    final edit to upload and host

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
 import listingRouter from './routes/listing.route.js'
+import path from 'path'
 dotenv.config();
 const app=express();
 app.use(express.json())
@@ -16,12 +17,16 @@ mongoose.connect(process.env.MONGODB).then(()=>{
   console.log("MongoDb Connected..")
 }).catch(e => console.log(e))
 
-
+  const __dirname = path.resolve();
 // Routes
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listingRouter)
 
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 // middleware
 app.use((err,req,res,next)=>{
   const statusCode=err.statusCode || 500; //internal server error :500
